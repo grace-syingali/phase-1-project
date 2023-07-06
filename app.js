99d6671b0d4a3e2d933b4f7fa90c1e2e059fdfec
grace <grace.syingali@student.moringaschool.com>
2023-07-06 14:33:56 +0300

    added an asynchronous function

--- app.js
+++ app.js
@@ -4,3 +4,16 @@ document.addEventListener('DOMContentLoaded', () => {
   
     randomJokeBtn.addEventListener('click', getRandomJoke);
   });
+  async function getRandomJoke() {
+    try {
+      const response = await fetch('https://api.example.com/jokes/random');
+      const data = await response.json();
+      const jokeContainer = document.getElementById('joke-container');
+  
+      if (jokeContainer) {
+        jokeContainer.innerHTML = data.joke;
+      }
+    } catch (error) {
+      console.error('Error fetching joke:', error);
+    }
+  }
